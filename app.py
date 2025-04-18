9573b8c1820a09880f0db261f1ebdbb71715e0c5
AndreiEverson <andreieverson7@gmail.com>
2025-04-18 19:28:34 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import os 
+
+restaurantes = ['pizza', 'sushi']
+
+def exibir_nome_do_programa():
+    print('🅢🅐🅑🅞🅡 🅔🅧🅟🅡🅔🅢🅢.\n')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalizar app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla parta voltar ao menu ')
+    main()
+
+def opcao_invalida():
+        print('opcao invalida\n')
+        voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('cadastro de novos restaurantes')
+    nome_do_restaurante = input('digite o nome do restaurante que desejas cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'o restaurante {nome_do_restaurante} foi cadastrado com sucesso')
+    voltar_ao_menu_principal()  
+
+def listar_restaurantes():
+    exibir_subtitulo('listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes ()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app() 
+        else:
+            opcao_invalida()
+    except: 
+        opcao_invalida()       
+    
+def main():
+  os.system('cls')
+  exibir_nome_do_programa()
+  exibir_opcoes()
+  escolher_opcao()
+
+if __name__ == '__main__':
+    main()
